e14cc49871bbf6f15f0e095a3d8ec867e8081b52
Faner217 <161946819+Faner217@users.noreply.github.com>
2025-09-08 16:34:16 +0000

    Deploying to gh-pages from @ Faner217/Faner217.github.io@918f6095af0500c4f98b5aad9b3f1e6849409e20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "Yifan Li is a Ph.D. candidate at the University of Tokyo. He is currently seeking a research internship.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
