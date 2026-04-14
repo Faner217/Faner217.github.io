e166bbe364fd6ec2678c8230d6cdc73427bd24fe
Faner217 <161946819+Faner217@users.noreply.github.com>
2026-04-14 02:27:04 +0000

    Deploying to gh-pages from @ Faner217/Faner217.github.io@1d98cc493922ecd01aa85fc0a3bde0c71f07e5e3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
@@ -68,8 +57,8 @@ ninja.data = [{
           section: "News",},{id: "news-i-m-going-to-apple-for-internship-see-you-in-shanghai",
           title: 'I’m going to Apple for internship! See you in Shanghai!',
           description: "",
-          section: "News",},{id: "news-see-you-in-chi-2026-barcelona-we-will-show-a-demo-of-our-lab",
-          title: 'See you in **CHI 2026** Barcelona! We will show a demo of our...',
+          section: "News",},{id: "news-see-you-in-chi2026-barcelona-we-will-show-a-demo-of-our-lab",
+          title: 'See you in **CHI2026** Barcelona! We will show a demo of our lab....',
           description: "",
           section: "News",},{id: "projects-picoring-mouse",
           title: 'picoRing mouse',
